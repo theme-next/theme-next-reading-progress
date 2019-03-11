f1dcdfc585f60cb1614c480c3b06a1632b1b6d8e
maple3142 <kirby741852963@gmail.com>
2019-03-11 21:54:22 +0800

    update percentage caculation logic

--- reading_progress.js
+++ reading_progress.js
@@ -1,31 +1,30 @@
-(function() {
-	if (!$(".reading-progress-bar").length) return; //no progress bar found
-	var supportsPassive = false;
+;(function() {
+	var $bar = $('.reading-progress-bar')
+	if (!$bar.length) return //no progress bar found
+	var supportsPassive = false
 	try {
-		var opts = Object.defineProperty({}, "passive", {
+		var opts = Object.defineProperty({}, 'passive', {
 			get: function() {
-				supportsPassive = true;
+				supportsPassive = true
 			}
-		});
-		window.addEventListener("test", $.noop, opts);
-		window.removeEventListener("test", $.noop);
+		})
+		window.addEventListener('test', $.noop, opts)
+		window.removeEventListener('test', $.noop)
 	} catch (e) {}
-
-	$(document).ready(function() {
-		var $bar = $(".reading-progress-bar");
+	var $w = $(window)
+	var $d = $(document)
+	$d.ready(function() {
 		window.addEventListener(
-			"scroll",
+			'scroll',
 			function() {
-				var $win = $(window);
-				var $post = $(".post-block");
-				var winmid = $win.scrollTop() + $win.height() / 2; //assume reader will focus on middle of screen(vertical)
-				var h = winmid - $post.position().top;
-				var percent = Math.round(h / $post.height() * 100);
-				if (percent < 0) percent = 0;
-				if (percent > 100) percent = 100;
-				$bar.css("width", percent + "%");
+				var wh = $w.height()
+				var dh = $d.height()
+				var st = $w.scrollTop()
+				var percent = (st / (dh - wh)) * 100
+				percent = percent > 100 ? 100 : percent < 0 ? 0 : percent
+				$bar.css('width', percent + '%')
 			},
 			supportsPassive ? { passive: true } : false
-		);
-	});
-})();
+		)
+	})
+})()
